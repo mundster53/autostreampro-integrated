69e786b489b6c100a6ab60d5bdc902b803a1d955
mundster53 <mundt53@gmail.com>
2025-07-13 14:31:34 -0500

    initiate R2 temporary storage

--- functions/stream-monitor.js
+++ functions/stream-monitor.js
@@ -74,6 +74,16 @@ class StreamMonitor {
         this.tokenExpiry = null;
         this.clipCreator = new ClipCreator(supabase);
         this.lastClipTime = {};
+
+        // Add this inside your existing constructor
+this.r2 = new S3Client({
+    region: 'auto',
+    endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+        accessKeyId: process.env.R2_ACCESS_KEY_ID,
+        secretAccessKey: process.env.R2_SECRET_ACCESS_KEY
+    }
+});
     }
 
     async getAppToken() {
